069810bf89c897e6688843fd891a8db0d124eb18
Tito-Arroyo <tito.arroyo@s.capregboces.org>
2024-03-19 13:14:49 -0400

    ex 10 prob 1

---
+++ ex10while loops/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ex10while_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+
+            while (input != 4)
+            {
+            Console.WriteLine("**************");
+            Console.WriteLine("menu");
+            Console.WriteLine("1 - new game");
+            Console.WriteLine("2 - load game");
+            Console.WriteLine("3 - options");
+            Console.WriteLine("4 - quit");
+            Console.WriteLine("**************");
+            Console.WriteLine();
+
+            //promt for and get user choice
+            Console.WriteLine("please enter a number shown");
+            input = int.Parse(Console.ReadLine());
+            Console.WriteLine(input);
+        }
+        }
+    }
+}
